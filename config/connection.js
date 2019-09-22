8a04e5ef380a07959817e8706d7d75ebf3dd9712
Orlando Buzana <orlandobuzana@outlook.com>
2019-09-22 00:24:35 -0400

    added routes and templates

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,7 @@ if (process.env.JAWSDB_MARIA_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_MARIA_URL);
 } else {
   connection = mysql.createConnection({
-    host: process.env.JAWSDB_MARIA_URL,
+    host: process.env.DB_HOST,
     port: 3306,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
