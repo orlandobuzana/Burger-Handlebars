4eb3df9e0598b24c502d32574b6e78a5a5bd90bf
Orlando Buzana <orlandobuzana@outlook.com>
2019-09-23 20:49:57 -0400

    added env pass & user

--- config/connection.js
+++ config/connection.js
@@ -3,11 +3,11 @@ if (process.env.JAWSDB_MARIA_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_MARIA_URL);
 } else {
   connection = mysql.createConnection({
-    host: "localhost",//process.env.DB_HOST,
+    host: process.env.DB_HOST,
     port: 3306,
-    user: "root",//process.env.DB_USER,
-    password: "Itapeva1998",//process.env.DB_PASS,
-    database: "burgers_db"//process.env.DB_NAME
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME
   });
 }
 connection.connect();
